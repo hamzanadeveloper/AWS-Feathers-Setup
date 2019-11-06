638fc80db6ae745298d8213b0b0492504d254517
hamzanadeveloper <hamza.arshad.developer@gmail.com>
2019-11-06 15:52:42 -0500

    Merged with feathers-react-starter

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -2,10 +2,9 @@
 
 const path = require('path')
 const webpack = require('webpack')
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const nodeExternals = require('webpack-node-externals')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const envalid = require('envalid')
 const { makeValidator } = envalid
 
@@ -19,12 +18,10 @@ const nonEmptyString = makeValidator((input) => {
 
 const stringifyKeys = obj => (sum, key) => Object.assign({}, sum, { [key]: JSON.stringify(obj[key]) })
 
-const isNodeModule = module => module.context && module.context.indexOf('node_modules') !== -1
-
 const PUBLIC_PATH = '/'
 
 module.exports = function (opts) {
-  const { wba, platform } = opts
+  const { platform } = opts
   const IS_BROWSER = platform === 'browser'
   const IS_SERVER = platform === 'node'
 
@@ -34,78 +31,50 @@ module.exports = function (opts) {
 
   const { str } = envalid
   const env = envalid.cleanEnv(
-    process.env,
-    Object.assign({
-        NODE_ENV: str({
-          choices: ['production', 'development'],
-          default: 'development',
-        }),
-        API_BASE_URL: str({ default: 'not provided' }),
-        SPA_BASE_URL: nonEmptyString(),
-      },
-        IS_BROWSER
-          ? { STATIC_ROOT: str({ default: 'not provided' })}
-          : { INTERNAL_API_BASE_URL: str({ default: 'not provided' }) }
-    ),
-    { strict: true  }
+      process.env,
+      Object.assign({
+            NODE_ENV: str({
+              choices: ['production', 'development'],
+              default: 'development',
+            }),
+            API_BASE_URL: str({ default: 'not provided' }),
+            SPA_BASE_URL: nonEmptyString(),
+          },
+          IS_BROWSER
+              ? { STATIC_ROOT: str({ default: 'not provided' })}
+              : { INTERNAL_API_BASE_URL: str({ default: 'not provided' }) }
+      ),
+      { strict: true  }
   )
-
-  console.log(`[webpack] Constructing configuration for platform ${platform}`)
-  console.log(`NODE_ENV: ${process.env.NODE_ENV}`)
-  console.log(`[webpack] API_BASE_URL=${env.API_BASE_URL}`)
-
-  if (IS_BROWSER) {
-    console.log(`[webpack] STATIC_ROOT=${env.STATIC_ROOT}`)
-  } else {
-    console.log(`[webpack] INTERNAL_API_BASE_URL=${env.INTERNAL_API_BASE_URL}`)
-  }
 
   /*
     PLUGINS
   */
 
-  const plugins = [
-    new webpack.DefinePlugin({
-      IS_BROWSER: JSON.stringify(IS_BROWSER),
-      IS_SERVER: JSON.stringify(IS_SERVER),
-    })
-  ]
+  const plugins = []
 
   if (IS_BROWSER) {
     plugins.push(
-      new webpack.DefinePlugin({
-        'process.env': [
-          'NODE_ENV',
-          'API_BASE_URL',
-          'SPA_BASE_URL',
-        ]
-          .reduce(stringifyKeys(env), {}),
-      }),
-      new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', minChunks: isNodeModule }),
-      new ExtractTextPlugin('styles.[hash].css'),
-      new HtmlWebpackPlugin({
-        favicon: './src/assets/favicon.ico',
-        filename: '_index.html',
-        template: './src/assets/template.html',
-        inject: false
-      })
-    )
-  }
-
-  if (IS_BROWSER && env.isProduction) {
-    plugins.push(
-      new webpack.LoaderOptionsPlugin({ minimize: true, debug: false }),
-      new webpack.optimize.UglifyJsPlugin({ sourceMap: true, comments: false })
-    )
-  }
-
-  if (IS_BROWSER && env.isProduction && wba) {
-    plugins.push( ...[new BundleAnalyzerPlugin()] )
-  }
-
-  if (IS_SERVER) {
-    plugins.push(
-      new webpack.BannerPlugin({ banner: 'require("source-map-support").install()', raw: true, entryOnly: false })
+        new webpack.DefinePlugin({
+          'process.env': [
+            'NODE_ENV',
+            'API_BASE_URL',
+            'SPA_BASE_URL',
+          ].reduce(stringifyKeys(env), {}),
+        }),
+        new MiniCssExtractPlugin({
+          // Options similar to the same options in webpackOptions.output
+          // all options are optional
+          filename: '[name].css',
+          chunkFilename: '[id].css',
+          ignoreOrder: false, // Enable to remove warnings about conflicting order
+        }),
+        new HtmlWebpackPlugin({
+          favicon: './src/assets/favicon.ico',
+          filename: '_index.html',
+          template: './src/assets/template.html',
+          inject: false
+        })
     )
   }
 
@@ -116,22 +85,37 @@ module.exports = function (opts) {
   const jsxRule = {
     test: /\.jsx?$/,
     exclude: /node_modules/,
-    loader: 'babel-loader',
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env']
+      }
+    }
   }
 
   const cssRule = Object.assign(
-    {
-      test: /\.css$/,
-    },
-    IS_BROWSER
-      ? { use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }) }
-      : { loader: 'css-loader/locals' }
+      {
+        test: /\.css$/,
+      },
+      IS_BROWSER
+          ? {
+            use: [
+              {
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                  hmr: process.env.NODE_ENV === 'development',
+                },
+              },
+              'css-loader',
+            ],
+          }
+          : { loader: 'css-loader/locals' }
   )
 
   const filesRule = {
     test: /\.(png|svg|gif|jpe?g)$/,
     loader: 'file-loader',
-    query: {
+    options: {
       emitFile: IS_BROWSER,
       publicPath: PUBLIC_PATH + 'images/',
       outputPath: 'images/',
@@ -144,10 +128,11 @@ module.exports = function (opts) {
   */
 
   const config = {
-    entry: IS_BROWSER ? [ 'babel-polyfill', './src/client.js' ] : './src/server.js',
+    mode: process.env.NODE_ENV,
+    entry: IS_BROWSER ? './src/client.js' : './src/server.js',
     devtool: env.isProduction
-      ? 'cheap-module-source-map'
-      : 'cheap-module-eval-source-map',
+        ? 'cheap-module-source-map'
+        : 'cheap-module-eval-source-map',
     output: {
       path: IS_BROWSER ? env.STATIC_ROOT : path.resolve(__dirname, 'bin'),
       publicPath: PUBLIC_PATH,
