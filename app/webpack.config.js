fc338f8a1c2247293b92849a05ed3d4e0f99c942
hamzanadeveloper <hamza.arshad.developer@gmail.com>
2019-11-08 15:32:09 -0500

    Testing out compressing vendor file

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -140,22 +140,8 @@ module.exports = function (opts) {
       libraryTarget: IS_BROWSER ? 'var' : 'commonjs2',
     },
     optimization: {
-      runtimeChunk: 'single',
       splitChunks: {
         chunks: 'all',
-        maxInitialRequests: 4,
-        minSize: 0,
-        cacheGroups: {
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name(module) {
-              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-
-              // npm package names are URL-safe, but some servers don't like @ symbols
-              return `npm.${packageName.replace('@', '')}`;
-            },
-          },
-        },
       },
     },
     target: IS_BROWSER ? 'web' : 'node',
