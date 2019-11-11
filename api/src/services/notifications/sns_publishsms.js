bce5da620664202a37eaf822b3521184f16e7bea
hamzanadeveloper <hamza.arshad.developer@gmail.com>
2019-11-11 12:32:47 -0500

    Added in pinpoint

--- api/src/services/notifications/sns_publishsms.js
+++ api/src/services/notifications/sns_publishsms.js
@@ -4,21 +4,63 @@ const AWS = require('aws-sdk');
 
 AWS.config.update({region: 'us-east-1', accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_KEY });
 
-module.exports = function (phone, message) {
-  console.log(phone)
+module.exports = function (phone, use_message) {
+  const originationNumber = "+15878096460";
+  const destinationNumber = phone;
+  const languageCode = "en-US";
+
+  const message = "This message was sent from Feathers-React-Starter "
+    + "using the AWS SDK for JavaScript in Node.js. Reply STOP to "
+    + "opt out.";
+
+  const applicationId = your-project-id;
+
+  const messageType = "TRANSACTIONAL";
+
+  AWS.config.update({region:'us-east-1'});
+
+  let pinpoint = new AWS.Pinpoint();
+
   const params = {
-    Message: message,
-    PhoneNumber: phone,
+    ApplicationId: applicationId,
+    MessageRequest: {
+      Addresses: {
+        [destinationNumber]: {
+          ChannelType: 'SMS'
+        }
+      },
+      MessageConfiguration: {
+        SMSMessage: {
+          Body: message,
+          MessageType: messageType,
+          OriginationNumber: originationNumber,
+        }
+      }
+    }
   };
 
-  const publishTextPromise = new AWS.SNS().publish(params).promise();
-
-  return publishTextPromise.then(
-    function(data) {
-        console.log("MessageID is " + data.MessageId);
-        console.log(data)
-    }).catch(
-    function(err) {
-        console.error(err, err.stack);
-    });
+  pinpoint.sendMessages(params, function(err, data) {
+    if(err) {
+      console.log(err.message);
+    } else {
+      console.log("Message sent! "
+        + data['MessageResponse']['Result'][destinationNumber]['StatusMessage']);
+    }
+  });
 }
+
+// const params = {
+//   Message: message,
+//   PhoneNumber: phone,
+// };
+//
+// const publishTextPromise = new AWS.SNS().publish(params).promise();
+//
+// return publishTextPromise.then(
+//   function(data) {
+//       console.log("MessageID is " + data.MessageId);
+//       console.log(data)
+//   }).catch(
+//   function(err) {
+//       console.error(err, err.stack);
+//   });
