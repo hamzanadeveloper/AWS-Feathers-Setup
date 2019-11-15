643d24a94bf3e15097a2e3a08ea6143711f89fd8
hamzanadeveloper <hamza.arshad.developer@gmail.com>
2019-11-15 15:13:46 -0500

    Ready to test responses

--- api/src/services/amazon/amazon.class.js
+++ api/src/services/amazon/amazon.class.js
@@ -7,23 +7,23 @@ const isSubscriptionConfirmationRequest = (params) => {
 }
 
 exports.Amazon = class Amazon extends Service {
-  create(data, params){
-    console.log(data)
-    console.log(params)
-
-    if(isSubscriptionConfirmationRequest(params)){
-      console.log("This is a subscription confirmation request.")
-
-      request.get(params['x-amz-sns-topic-arn'], (err, res, body) => {
-        console.log("[INFO] GET on SNS subscription URL: " + data.SubscribeURL)
-
-        if(err){
-          reject(new errors.BadRequest("GET on subscription URL failed"))
-        } else {
-          console.log("[INFO] SNS subscription confirmed")
-        }
-      })
-
-    }
-  }
+  // create(data, params){
+  //   console.log(data)
+  //   console.log(params)
+  //
+  //   if(isSubscriptionConfirmationRequest(params)){
+  //     console.log("This is a subscription confirmation request.")
+  //
+  //     request.get(params['x-amz-sns-topic-arn'], (err, res, body) => {
+  //       console.log("[INFO] GET on SNS subscription URL: " + data.SubscribeURL)
+  //
+  //       if(err){
+  //         reject(new errors.BadRequest("GET on subscription URL failed"))
+  //       } else {
+  //         console.log("[INFO] SNS subscription confirmed")
+  //       }
+  //     })
+  //
+  //   }
+  // }
 };
