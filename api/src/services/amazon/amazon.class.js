e11838be51485af2c0d57c48d8e06ff49001c2bb
hamzanadeveloper <hamza.arshad.developer@gmail.com>
2019-11-15 14:00:25 -0500

    Testing out subscription confirmation

--- api/src/services/amazon/amazon.class.js
+++ api/src/services/amazon/amazon.class.js
@@ -1,8 +1,4 @@
 const { Service } = require('feathers-sequelize');
 
 exports.Amazon = class Amazon extends Service {
-  create(data, params){
-    console.log(data)
-    console.log(params)
-  }
 };
--- api/src/services/amazon/amazon.hooks.js
+++ api/src/services/amazon/amazon.hooks.js
@@ -1,10 +1,18 @@
 
+const { iff } = require("feathers-hooks-common")
+
+const isSubscriptionConfirmationRequest = (context) => {
+  return context.params.headers["x-amz-sns-message-type"] === "SubscriptionConfirmation"
+}
+
 module.exports = {
   before: {
     all: [],
     find: [],
     get: [],
-    create: [],
+    create: [
+      iff(isSubscriptionConfirmationRequest, console.log("Whats up") )
+    ],
     update: [],
     patch: [],
     remove: []
