dc8e4a85c04498c748118fddc7a757b34d4a635d
hamzanadeveloper <hamza.arshad.developer@gmail.com>
2019-11-15 13:41:13 -0500

    Logging requests

--- api/src/services/amazon/amazon.class.js
+++ api/src/services/amazon/amazon.class.js
@@ -1,4 +1,8 @@
 const { Service } = require('feathers-sequelize');
 
 exports.Amazon = class Amazon extends Service {
+  create(data, params){
+    console.log(data)
+    console.log(params)
+  }
 };
