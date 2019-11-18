de39e25eb94edc161ddc48ad8765b8dcaa88da24
hamzanadeveloper <hamza.arshad.developer@gmail.com>
2019-11-18 11:01:28 -0500

    Changing up the mapping

--- api/src/services/amazon/parse-response-contents.js
+++ api/src/services/amazon/parse-response-contents.js
@@ -4,6 +4,6 @@ module.exports  = () => function (context) {
   console.log("Llama")
   console.log(context.data.Message)
 
-  const new_context_data = Object.assign({}, context.data, ...context.data.Message);
+  const new_context_data = Object.assign({}, context.data, context.data.Message);
   console.log(new_context_data)
 }
