b2e9fc931651735d4dafae65e17be0f1858aedb0
hamzanadeveloper <hamza.arshad.developer@gmail.com>
2019-11-18 10:57:30 -0500

    Changing up the mapping of the message

--- api/src/services/amazon/parse-response-contents.js
+++ api/src/services/amazon/parse-response-contents.js
@@ -1,9 +1,9 @@
 'use strict'
 
 module.exports  = () => function (context) {
-  console.log("Alpaca")
+  console.log("Llama")
   console.log(context.data.Message)
 
-  const result = Object.assign({}, context, ...context.data.Message);
-  console.log(result)
+  const new_context_data = Object.assign({}, context.data, ...context.data.Message);
+  console.log(new_context_data)
 }
