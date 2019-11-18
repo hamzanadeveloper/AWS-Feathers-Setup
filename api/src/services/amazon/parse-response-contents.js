ce98f37d27a7b7e19d8518a5625011cfc1c35742
hamzanadeveloper <hamza.arshad.developer@gmail.com>
2019-11-18 11:04:14 -0500

    Using spread in object literals

--- api/src/services/amazon/parse-response-contents.js
+++ api/src/services/amazon/parse-response-contents.js
@@ -4,6 +4,6 @@ module.exports  = () => function (context) {
   console.log("Llama")
   console.log(context.data.Message)
 
-  const new_context_data = Object.assign({}, context.data, context.data.Message);
+  const new_context_data = { ...context.data.Message, ...context.data};
   console.log(new_context_data)
 }
