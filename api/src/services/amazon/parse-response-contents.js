dece4cc3d3734cb61006f3becb0d8fc1e36d7f17
hamzanadeveloper <hamza.arshad.developer@gmail.com>
2019-11-18 11:09:45 -0500

    Parse the message object into JSON

--- api/src/services/amazon/parse-response-contents.js
+++ api/src/services/amazon/parse-response-contents.js
@@ -1,9 +1,10 @@
 'use strict'
 
 module.exports  = () => function (context) {
-  console.log("Llama")
+  console.log("Alpaca")
   console.log(context.data.Message)
 
-  const new_context_data = { ...context.data.Message, ...context.data};
+  const messageData = JSON.parse(context.data.Message)
+  const new_context_data = { ...messageData, ...context.data};
   console.log(new_context_data)
 }
