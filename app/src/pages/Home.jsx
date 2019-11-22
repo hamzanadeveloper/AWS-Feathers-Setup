89b9cb0fd85707b309f5e3174d57115f5265fa18
hamzanadeveloper <hamza.arshad.developer@gmail.com>
2019-11-22 14:05:17 -0500

    Added logout and authentication to home

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Redirect } from "react-router-dom";
 import clsx from 'clsx'
 import { makeStyles } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
@@ -122,10 +123,24 @@ const useStyles = makeStyles(theme => ({
 
 export default function Dashboard() {
     const classes = useStyles()
+    const [newMessage, setMessage] = useState('')
     const [open, setOpen] = useState(true)
+    const [redirect, setRedirect] = useState(false)
+    const [isAuthenticated, setAuthentication] = useState(true)
     const [currentRecipient, setCurrRecipient] = useState({})
     const [messageHistory, setMessageHistory] = useState([])
-    const [newMessage, setMessage] = useState('')
+
+    useEffect(() => {
+        app.authentication.getAccessToken()
+            .then(accessToken => {
+                if (accessToken) {
+                    return app.reAuthenticate()
+                        .then(() => setAuthentication(true))
+                } else {
+                    setAuthentication(false)
+                }
+            })
+    })
 
     const handleDrawerOpen = () => {
         setOpen(true)
@@ -147,11 +162,24 @@ export default function Dashboard() {
             })
     }
 
+    const handleLogOut = () => {
+        app.logout()
+            .then(() => setRedirect(true))
+    }
+
     return (
         <div className={classes.root}>
+
+            { redirect || !isAuthenticated ? <Redirect to="/" /> : null }
+
             <CssBaseline />
             <TopBar open={open} handleDrawerOpen={handleDrawerOpen} recipientName={currentRecipient.name}/>
-            <SideDrawer drawerOpen={open} handleDrawerClose={handleDrawerClose} switchToNewRecipient={switchToNewRecipient}/>
+            <SideDrawer
+                drawerOpen={open}
+                handleDrawerClose={handleDrawerClose}
+                switchToNewRecipient={switchToNewRecipient}
+                handleLogOut={handleLogOut}/>
+
                 <main className={classes.content}>
                     <div className={classes.appBarSpacer} />
 
@@ -159,8 +187,7 @@ export default function Dashboard() {
                         currentRecipient={currentRecipient}
                         messageHistory={messageHistory}
                         newMessage={newMessage}
-                        open={open}
-                    />
+                        open={open}/>
                 </main>
             }
         </div>
@@ -324,6 +351,14 @@ const SideDrawer = props => {
                 )}
             </div>
             <Divider />
+            <div style={{ textAlign: 'center', position: 'absolute', bottom: 10, width: '100%' }}>
+                { open ?
+                    <Button variant="contained" color="secondary" fullwidth onClick={() => props.handleLogOut()} >
+                        Logout
+                    </Button>
+                    : null
+                }
+            </div>
         </Drawer>
     )
 }
