e0175d7bc35a5451e4edc601bb61c3f30ba26f6e
hamzanadeveloper <hamza.arshad.developer@gmail.com>
2019-11-25 12:19:53 -0500

    Added the Helmet

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { Helmet } from 'react-helmet'
 
 import responsive from 'FRS/components/responsive.jsx'
 import LandingPage from 'FRS/pages/LandingPage.jsx'
 import Home from 'FRS/pages/Home.jsx'
+
+const TITLE = 'Insight POC'
 
 @responsive
 export default class App extends Component {
   render() {
     return (
         <div>
+            <Helmet>
+                <title>{ TITLE }</title>
+            </Helmet>
             <BrowserRouter>
                 <Switch>
                     <Route path="/home" component={Home} />
