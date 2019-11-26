9cd8180682dffbf58ceadbcaefdf82f0cae32070
hamzanadeveloper <hamza.arshad.developer@gmail.com>
2019-11-26 13:46:11 -0500

    Added the height to the button:

--- app/src/components/ChatBox.jsx
+++ app/src/components/ChatBox.jsx
@@ -22,6 +22,7 @@ const useStyles = makeStyles(theme => ({
         width: '100%',
     },
     button: {
+        height: '40px',
         margin: theme.spacing(1),
         marginBottom: '13px'
     },
